601888e4a56d710a4116d4dd2aab35e208fe0bb4
bellaciaodowhile <125389712+bellaciaodowhile@users.noreply.github.com>
2025-06-17 18:15:53 -0400

    feat: add geocoding

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import * as XLSX from 'xlsx'; // Importa la biblioteca xlsx
+import * as XLSX from 'xlsx'; 
 import { createClient } from '@supabase/supabase-js';
+import 'leaflet-control-geocoder';
 
 const SUPABASE_URL = 'https://qmzmznpbpvonegajtavg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtem16bnBicHZvbmVnYWp0YXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMjE5NjMsImV4cCI6MjA2NTU5Nzk2M30.YkJmlrS_55zqdJ-Iu9esXJO56LfJwg-itB6IwGUJnA8';
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Define the marker icons
 const createMarkerIcon = (isFull) => {
   return L.icon({
     iconUrl: isFull
-      ? 'https://static.vecteezy.com/system/resources/previews/013/760/669/non_2x/map-location-pin-icon-in-red-colors-png.png' // URL del pin rojo
-      : 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Map_pin_icon_green.svg/752px-Map_pin_icon_green.svg.png', // URL del pin verde
+      ? 'https://static.vecteezy.com/system/resources/previews/013/760/669/non_2x/map-location-pin-icon-in-red-colors-png.png'
+      : 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Map_pin_icon_green.svg/752px-Map_pin_icon_green.svg.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
@@ -27,50 +27,38 @@ const center = {
 };
 
 const MapComponent = () => {
+  const [isLoading, setIsLoading] = useState(false); 
   const [markers, setMarkers] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredMarkers, setFilteredMarkers] = useState([]);
-  
+  const [geocodeResults, setGeocodeResults] = useState([]);
+  const mapRef = React.useRef();
+
   useEffect(() => {
-  const fetchMarkers = async () => {
-    // Obtener FATs
-    const { data: fats, error: fatsError } = await supabase
-      .from('fats')
-      .select('*');
-    
-    if (fatsError) {
-      console.error('Error fetching FATs:', fatsError);
-      return;
-    }
-
-    // Obtener Clientes
-    const { data: clientes, error: clientesError } = await supabase
-      .from('clientes') // Asegúrate de que el nombre de la tabla sea correcto
-      .select('*');
-    
-    if (clientesError) {
-      console.error('Error fetching Clientes:', clientesError);
-      return;
-    }
-
-    // Asociar Clientes a FATs
-    const fatsWithClients = fats.map(fat => {
-      const associatedClients = clientes.filter(cliente => {console.log(cliente.fat_id);return cliente.fat_id == fat.id});
-      console.log(fat.id)
-      return {
-        ...fat,
-        clientes: associatedClients, // Agregar clientes asociados al FAT
-      };
-    });
-    console.log(fatsWithClients)
-
-    setMarkers(fatsWithClients);
-    setFilteredMarkers(fatsWithClients); // Inicializa los marcadores filtrados
-  };
-
-  fetchMarkers();
-}, []);
+    const fetchMarkers = async () => {
+      const { data: fats, error: fatsError } = await supabase.from('fats').select('*');
+      if (fatsError) {
+        console.error('Error fetching FATs:', fatsError);
+        return;
+      }
+      const { data: clientes, error: clientesError } = await supabase.from('clientes').select('*');
+      if (clientesError) {
+        console.error('Error fetching Clientes:', clientesError);
+        return;
+      }
+      const fatsWithClients = fats.map(fat => {
+        const associatedClients = clientes.filter(cliente => cliente.fat_id === fat.id);
+        return {
+          ...fat,
+          clientes: associatedClients,
+        };
+      });
+      setMarkers(fatsWithClients);
+      setFilteredMarkers(fatsWithClients);
+    };
+    fetchMarkers();
+  }, []);
 
   const handleFileUpload = async (event) => {
     const file = event.target.files[0];
@@ -179,33 +167,74 @@ const MapComponent = () => {
 
   const handleSidebarClick = (marker) => {
     setSelectedMarker(marker);
-    console.log({
-      click: 'test',
-      coord: {
-        lat: parseFloat(marker.lat),
-        lng: parseFloat(marker.lng)
-      }
-    })
-    mapRef.current.flyTo([parseFloat(marker.lat), parseFloat(marker.lng)], 14); // Centra el mapa en el marcador
+    mapRef.current.flyTo([parseFloat(marker.lat), parseFloat(marker.lng)], 14);
   };
 
-  const handleSearch = (term) => {
-  setSearchTerm(term);
-  const filtered = markers.filter(marker => {
-    // Busca en el IdFat
-    const matchesFat = marker.IdFat.toLowerCase().includes(term.toLowerCase());
+ const handleSearch = (term) => {
+    setSearchTerm(term);
+    const filtered = markers.filter(marker => {
+      const matchesFat = marker.IdFat.toLowerCase().includes(term.toLowerCase());
+      const matchesClient = marker.clientes && marker.clientes.some(cliente => 
+        cliente.nombreApellido.toLowerCase().includes(term.toLowerCase()) || 
+        cliente.cedulaRiff.toLowerCase().includes(term.toLowerCase())
+      );
+      return matchesFat || matchesClient;
+    });
+    setFilteredMarkers(filtered);
+  };
 
-    // Busca en los clientes asociados
-    const matchesClient = marker.clientes && marker.clientes.some(cliente => 
-      cliente.nombreApellido.toLowerCase().includes(term.toLowerCase()) || 
-      cliente.cedulaRiff.toLowerCase().includes(term.toLowerCase())
-    );
+  const handleGeocode = (address) => {
+    const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=jsonv2`;
 
-    // Devuelve verdadero si coincide en cualquiera de los dos
-    return matchesFat || matchesClient;
-  });
-  setFilteredMarkers(filtered);
-};
+    // Eliminar los marcadores existentes del mapa
+    geocodeResults.forEach(marker => {
+      console.log(marker)
+        mapRef.current.removeLayer(marker.marker); // Asegúrate de que 'marker.marker' sea el objeto correcto
+    });
+
+    console.log(geocodeResults);
+    setGeocodeResults([]);
+    setIsLoading(true); // Iniciar el preloader
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error en la red');
+        }
+        return response.json();
+      })
+      .then(results => {
+        if (results && results.length > 0) {
+          const markers = results.map(result => {
+            const { lat, lon, display_name } = result;
+            const newMarker = L.marker([lat, lon]).addTo(mapRef.current); // Crear el marcador y añadirlo al mapa
+            return {
+              id: generateUUID(),
+              IdFat: display_name,
+              lat,
+              lng: lon,
+              description: 'Ubicación buscada',
+              totalPorts: 0,
+              clientes: [],
+              marker: newMarker // Guardar referencia del marcador
+            };
+          });
+          setGeocodeResults(markers); // Actualiza los resultados de geocodificación con todos los marcadores
+
+          // Opcional: Si deseas centrar el mapa en el primer resultado
+          const { lat, lon } = results[0];
+          mapRef.current.flyTo([lat, lon], 14);
+        } else {
+          // alert('No se encontró la dirección. Por favor, intenta con otro formato.');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('Ocurrió un error al buscar la dirección.');
+      }).finally(() => {
+        setIsLoading(false); // Detener el preloader
+      });
+  }
 
   const generateUUID = () => {
     var d = new Date().getTime();
@@ -217,20 +246,16 @@ const MapComponent = () => {
     return uuid;
   };
 
-  const mapRef = React.useRef();
-
   return (
     <div className='flex'>
-      {/* Sidebar */}
       <div className='w-1/4 bg-white shadow-2xl p-4 h-screen overflow-y-auto'>
         <h2 className='text-lg font-bold mb-4'>Locations</h2>
-         {/* File Upload Button */}
         <div className='mb-4'>
           <input
             type='file'
             accept='.xlsx, .xls'
             onChange={handleFileUpload}
-            className='hidden' // Ocultar el input de archivo
+            className='hidden'
             id='file-upload'
           />
           <label htmlFor='file-upload' className='w-full p-2 text-center bg-blue-500 text-white font-bold uppercase rounded-lg cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 inline-block'>
@@ -238,7 +263,6 @@ const MapComponent = () => {
           </label>
           <h4 className='uppercase text-xs my-1 font-bold text-gray-500'>importarás fats con sus respectivos clientes.</h4>
         </div>
-        {/* Search Bar */}
         <div className='mb-4'>
           <input
             type='text'
@@ -247,7 +271,6 @@ const MapComponent = () => {
             onChange={(e) => handleSearch(e.target.value)}
           />
         </div>
-        {/* Locations List */}
         <ul>
           {filteredMarkers.map(marker => (
             <li
@@ -263,8 +286,7 @@ const MapComponent = () => {
           ))}
         </ul>
       </div>
-      {/* Map */}
-      <div className='w-3/4'>
+      <div className='relative w-3/4'>
         <MapContainer center={center} zoom={10} style={{ height: '100vh', width: '100%' }} ref={mapRef}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -313,6 +335,42 @@ const MapComponent = () => {
             </Marker>
           ))}
         </MapContainer>
+        {/* Lista flotante de resultados de geocodificación */}
+         <div className='absolute right-0 top-0 bg-white shadow-lg p-4 h-screen overflow-y-auto w-1/4 z-[999999999]'>
+          <h3 className='text-lg font-bold mb-4'>Resultados de Geocodificación</h3>
+          <div className='mb-4'>
+            <input
+              type='text'
+              placeholder='Buscar dirección...'
+              className='w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  handleGeocode(e.target.value);
+                }
+              }}
+            />
+          </div>
+          {isLoading ? ( // Mostrar preloader si está cargando
+            <div className='flex justify-center items-center'>
+              <div className='loader'>Espere un momento...</div> {/* Aquí puedes usar un spinner o un texto de carga */}
+            </div>
+          ) : (
+            <ul>
+              {geocodeResults.map(result => (
+                <li
+                  key={result.id}
+                  className='cursor-pointer mb-2 p-2 rounded-lg hover:bg-blue-100 transition duration-200 font-semibold flex items-center gap-2'
+                  onClick={() => {
+                    mapRef.current.flyTo([result.lat, result.lng], 14);
+                    setSelectedMarker(result);
+                  }}
+                >
+                  <span className='w-[10px] h-[10px] bg-blue-500 rounded-full inline-block'></span> {result.IdFat}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
